a62add1315724f38b4d120dc0dccc4f150b5f651
de-visser <81836362+de-visser@users.noreply.github.com>
2021-12-23 17:29:39 +0100

    a0

--- timer.js
+++ timer.js
@@ -6,7 +6,7 @@ console.log(size);
 function updateTimer() {
 
 	// yyyy-MM-dd HH:mm:ss
-  past  = Date.parse("2021-12-21 00:00:00");
+  past  = Date.parse("2021-12-23 17:29:00");
   now     = new Date();
   diff    = now - past;
 
