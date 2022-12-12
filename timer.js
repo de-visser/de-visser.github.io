bcce55780b8a938b0c5868307c990cb4a1745ab0
de-visser <81836362+de-visser@users.noreply.github.com>
2022-12-12 19:05:01 +0100

    Update timer.js

--- timer.js
+++ timer.js
@@ -6,7 +6,7 @@ console.log(size);
 function updateTimer() {
 
 	// yyyy-MM-dd HH:mm:ss
-  past  = Date.parse("2022-12-12 16:56:00");
+  past  = Date.parse("2022-12-12 19:00:00");
   now     = new Date();
   diff    = now - past;
 
